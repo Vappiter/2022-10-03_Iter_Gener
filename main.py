f518b443c4abb37efa95ba4f2d2fd12881962256
Vappiter <vappiter@yandex.ru>
2022-10-04 12:16:57 +0300

    Job end + option 2 Generator RIGHT

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from traitlets import Instance
-
 
 nested_list = [
 	['a', ['q',True, [33,'dsad'],False],'b', 'c', [3,9,18]],
@@ -30,20 +28,33 @@ class FlatIterator():
      return self.one_list [self.cursor] 
     else:
         raise StopIteration 
-      
-def my_generator(lst):
-  test_lst = flat_list(lst)
+
+#  Version 1 NOT RIGHT
+
+# def my_generator(lst):
+#   test_lst = flat_list(lst)
+#   var1 = 0
+#   while var1 < len(test_lst):
+#     yield test_lst[var1]
+#     var1 += 1
+
+#  Version 2 RIGHT  (Подсмотрено в DISCORD. Генератор в рекурсии нужно вызывать, как генератор)
+
+def my_generator(test_lst):
   var1 = 0
-  while var1 < len(test_lst):
-    yield test_lst[var1]
-    var1 += 1
-    
+  for var1 in test_lst:
+    if not isinstance (var1, list):
+     yield var1 
+    else:
+      for var2 in (my_generator (var1)):
+        yield var2
+
 if __name__ == '__main__': 
  for item in FlatIterator(nested_list):
    print(item)
  my_flat_list = [item for item in FlatIterator(nested_list)]  
  print (my_flat_list)
-test = my_generator(nested_list)
-print (test)
-for item1 in test:
+# test = my_generator(nested_list)
+# print (test)
+for item1 in my_generator(nested_list):
   print (item1)  
